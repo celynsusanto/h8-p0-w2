4328d68038e78fc4dbd4692c98ca4c63a7d92fb1
Celyn Susanto <celyn.vy@gmail.com>
2018-08-29 18:05:43 +0700

    Add files via upload

---
+++ Week 2-2.3.js
@@ -0,0 +1,64 @@
+var Hari= "6";
+//tanggal 1-31
+var Bulan= "9";
+//bulan 1-12
+var Tahun= "1996";
+//tahun 1900-2200
+
+switch (Hari) {
+  default:
+  console.log(Hari)
+  break;
+}
+
+switch (Bulan) {
+  case "1":
+  console.log("Januari");
+  break;
+  case "2":
+  console.log("Februari");
+  break;
+  case "3":
+  console.log("Maret");
+  break;
+  case "4":
+  console.log("April");
+  break;
+  case "5":
+  console.log("Mei");
+  break;
+  case "6":
+  console.log("Juni");
+  break;
+  case "7":
+  cosnsole.log("Juli");
+  break;
+  case "8":
+  console.log("Agustus");
+  break;
+  case "9":
+  console.log("September");
+  break;
+  case "10":
+  console.log("Oktober");
+  break;
+  case "11":
+  console.log("November");
+  break;
+  case "12":
+  console.log("Desember");
+  break;
+  default:
+  console.log("Invalid");
+  break;
+}
+
+switch (Tahun) {
+  default:
+  console.log(Tahun)
+  break;
+}
+
+
+
+
