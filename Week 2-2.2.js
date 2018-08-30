9cd16bb0f62caa4ea231fd50ac0918e9a30908b4
Celyn Susanto <celyn.vy@gmail.com>
2018-08-30 10:27:25 +0700

    Add files via upload

--- Week 2-2.2.js
+++ Week 2-2.2.js
@@ -1,14 +1,14 @@
-var Nama= "Zero";
-var Peran= "Penyihir";
+var Nama= "";
+var Peran= "";
 
 if (Nama=="") {
   console.log("Nama harus diisi!");
-} else if (Nama== "Mikael" && Peran== "") {
-  console.log("Halo Mikael, Pilih peranmu untuk memulai game!");
-} else if (Nama== "Nina" && Peran== "Ksatria") {
-  console.log("Selamat datang di Dunia Proxytia, Nina " + "\n" + "Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!");
-} else if (Nama== "Danu" && Peran== "Tabib") {
-  console.log("Selamat datang di Dunia Proxytia, Danu" + "\n" + "Halo Tabib Danu, kamu akan membantu temanmu yang terluka.");
-} else if (Nama== "Zero" && Peran== "Penyihir") {
-  console.log("Selamat datang di Dunia Proxytia, Zero " + "\n" + "Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!")
+} else if (Peran== "") {
+  console.log("Halo "+ Nama + ", Pilih peranmu untuk memulai game!");
+} else if (Peran== "Ksatria") {
+  console.log("Selamat datang di Dunia Proxytia,  " + Nama + "\n" + "Halo Ksatria " + Nama + ", kamu dapat menyerang dengan senjatamu!");
+} else if (Peran== "Tabib") {
+  console.log("Selamat datang di Dunia Proxytia,  " + Nama + "\n" + "Halo Tabib " + Nama + ", kamu akan membantu temanmu yang terluka.");
+} else if (Peran== "Penyihir") {
+  console.log("Selamat datang di Dunia Proxytia,  " + Nama + "\n" + "Halo Penyihir " + Nama + ", ciptakan keajaiban yang membantu kemenanganmu!")
 }
