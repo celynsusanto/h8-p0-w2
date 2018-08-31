e18325643fcdb244c19e9629f0a75a7cae960cfc
Celyn Susanto <celyn.vy@gmail.com>
2018-08-31 21:30:30 +0700

    Add files via upload

--- Week 2-3.1.js
+++ Week 2-3.1.js
@@ -29,11 +29,11 @@ console.log('Fifth Word: ' + FifthWord);
 
 //3.
 var word3 = 'wow JavaScript is so cool';
-var exampleFirstWord3 = word.substring(0, 3);
-var secondWord3 = word.substring(4, 14); 
-var thirdWord3 = word.substring(15, 17); 
-var fourthWord3 = word.substring(18, 20); 
-var fifthWord3 = word.substring(21, 25);
+var exampleFirstWord3 = word3.substring(0, 3);
+var secondWord3 = word3.substring(4, 14); 
+var thirdWord3 = word3.substring(15, 17); 
+var fourthWord3 = word3.substring(18, 20); 
+var fifthWord3 = word3.substring(21, 25);
 
 console.log('First Word: ' + exampleFirstWord3);
 console.log('Second Word: ' + secondWord3);
@@ -44,11 +44,11 @@ console.log('Fift Word: ' + fifthWord3);
 
 //4.
 var word4 = 'wow JavaScript is so cool';
-var exampleFirstWord4 = word.substring(0, 3);
-var secondWord4 = word.substring(4, 14); 
-var thirdWord4 = word.substring(15, 17); 
-var fourthWord4 = word.substring(18, 20); 
-var fifthWord4 = word.substring(21, 25);
+var exampleFirstWord4 = word4.substring(0, 3);
+var secondWord4 = word4.substring(4, 14); 
+var thirdWord4 = word4.substring(15, 17); 
+var fourthWord4 = word4.substring(18, 20); 
+var fifthWord4 = word4.substring(21, 25);
 
 var firstWordLength = exampleFirstWord4.length;
 var secondWordLength = secondWord4.length;
