25e98cb337f96a63070f9add66005431e7bc22b1
Celyn Susanto <celyn.vy@gmail.com>
2018-09-01 18:51:21 +0700

    Add files via upload

--- Week 2-2.3.js
+++ Week 2-2.3.js
@@ -6,57 +6,70 @@ var Tahun = 1996;
 //tahun 1900-2200;
 
 
-if (Hari > 31 || Hari < 1) {
-  Hari = " Invalid Date";
-} else if (Hari <= 31 && Hari >= 1) {
-  Hari = Hari;
+switch (true) {
+  case Hari > 31:
+    Hari = " Invalid Day ";
+    break;
+  case Hari < 1:
+    Hari = " Invalid Day ";
+    break;
+  default:
+    Hari = Hari;
+    break;
 }
-if (Bulan <= 12) {
-  switch (Bulan) {
-    case 1:
-      Bulan = " Januari ";
-      break;
-    case 2:
-      Bulan = " Februari ";
-      break;
-    case 3:
-      Bulan = " Maret ";
-      break;
-    case 4:
-      Bulan = " April ";
-      break;
-    case 5:
-      Bulan = " Mei ";
-      break;
-    case 6:
-      Bulan = " Juni ";
-      break;
-    case 7:
-      Bulan = " Juli ";
-      break;
-    case 8:
-      Bulan = " Agustus ";
-      break;
-    case 9:
-      Bulan = " September ";
-      break;
-    case 10:
-      Bulan = " Oktober ";
-      break;
-    case 11:
-      Bulan = " November ";
-      break;
-    case 12:
-      Bulan = " Desember ";
-      break;
-  }
-} else if (Bulan > 12) {
-  Bulan = " Invalid Month "
+
+switch (Bulan) {
+  case 1:
+    Bulan = " Januari ";
+    break;
+  case 2:
+    Bulan = " Februari ";
+    break;
+  case 3:
+    Bulan = " Maret ";
+    break;
+  case 4:
+    Bulan = " April ";
+    break;
+  case 5:
+    Bulan = " Mei ";
+    break;
+  case 6:
+    Bulan = " Juni ";
+    break;
+  case 7:
+    Bulan = " Juli ";
+    break;
+  case 8:
+    Bulan = " Agustus ";
+    break;
+  case 9:
+    Bulan = " September ";
+    break;
+  case 10:
+    Bulan = " Oktober ";
+    break;
+  case 11:
+    Bulan = " November ";
+    break;
+  case 12:
+    Bulan = " Desember ";
+    break;
+  default:
+    Bulan = " Invalid Month "
 }
-if (Tahun >= 1900 && Tahun <= 2200) {
-  Tahun = Tahun
-} else if (Tahun < 1900 || Tahun > 2200) {
-  Tahun = " Invalid Year "
+
+
+switch (true) {
+  case Tahun < 1900:
+    Tahun = " Invalid Year ";
+    break;
+  case Tahun > 2200:
+    Tahun = " Invalid Year ";
+    break;
+  default:
+    Tahun = Tahun;
+    break;
 }
 
 console.log(Hari + Bulan + Tahun)
