fb6beaec9813dd35aba04d66df57bfdd58e818cb
Celyn Susanto <celyn.vy@gmail.com>
2018-09-01 19:27:40 +0700

    Add files via upload

--- Week 2-5.4.js
+++ Week 2-5.4.js
@@ -1,16 +1,16 @@
-function xo(str){
-  var count1=str.split('x').length;
-  var count2=str.split('o').length;
-  if(count1==count2){
-    return("true")
-  }else {
-    return("false")
+function xo(str) {
+  var count1 = str.split('x').length-1;
+  var count2 = str.split('o').length-1;
+  if (count1 == count2) {
+    return ("true")
+  } else {
+    return ("false")
   }
 }
 
 console.log(xo('xoxoxo'));
-console.log(xo('oxooxo')); 
-console.log(xo('oxo')); 
-console.log(xo('xxxooo')); 
-console.log(xo('xoxooxxo')); 
+console.log(xo('oxooxo'));
+console.log(xo('oxo'));
+console.log(xo('xxxooo'));
+console.log(xo('xoxooxxo'));
 
