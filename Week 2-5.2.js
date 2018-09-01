06efd84320aa19dfdcd0eab46a83bd3835f9eb40
Celyn Susanto <celyn.vy@gmail.com>
2018-09-01 16:29:18 +0700

    Add files via upload

--- Week 2-5.2.js
+++ Week 2-5.2.js
@@ -1,12 +1,13 @@
-var array
 function balikKata(kata){
-  array=kata.split("");
-  array.reverse();
-  kata=array.join("");
-  return kata
+  var katabaru=""
+  for(var i=kata.length-1; i>=0; i--)
+  katabaru=katabaru+kata[i]
+  return katabaru
 }
+
 console.log(balikKata("Hello World and Coders"));
 console.log(balikKata('John Doe'));
 console.log(balikKata('I am a bookworm'));
 console.log(balikKata('Coding is my hobby'));
 console.log(balikKata('Super'));
+
