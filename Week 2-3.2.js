e9e42b1e5d99d679025cadd1cc7796b471f461a2
Celyn Susanto <celyn.vy@gmail.com>
2018-09-02 13:06:07 +0700

    Add files via upload

--- Week 2-3.2.js
+++ Week 2-3.2.js
@@ -1,62 +1,65 @@
 //1.
 //LOOPING PERTAMA
-var statement=0
-var nomor=2
-while(statement<10, nomor<=20){
-  console.log(nomor + ' - I Love Coding')
-  statement=statement+1
-  nomor=nomor+2
+var nomor = 2
+var kalimat = ' - I Love Coding'
+while (nomor <= 20) {
+  console.log(nomor + kalimat)
+  nomor = nomor + 2
 }
 
 //LOOPING KEDUA
-var statement1=10
-var nomor1=20
-while(statement1>0, nomor1>=2){
-  console.log(nomor1 + ' - I will become fullstack developer')
-  statement1=statement1-1
-  nomor1=nomor1-2
+var nomor1 = 20
+var kalimat1 = ' - I will become fullstack developer'
+while (nomor1 >= 2) {
+  console.log(nomor1 + kalimat1)
+  nomor1 = nomor1 - 2
 }
 
 
 //2.
 //LOOPING PERTAMA
-for (statement=1, nomor=1; statement<=20, nomor<=20; statement++, nomor++){
-  console.log(nomor + ' - I Love Coding')
+
+for (nomor = 1; nomor <= 20; nomor++) {
+  var kalimat2 = ' - I Love Coding'
+  console.log(nomor + kalimat2)
 }
+
 //LOOPING KEDUA
-for(statement=20, nomor=20; statement>0, nomor>0; statement--, nomor--){
-  console.log(nomor + ' - I Will Become Fullstack Developer')
+
+for (nomor = 20; nomor > 0; nomor--) {
+  var kalimat3 = ' - I Will Become Fullstack Developer'
+  console.log(nomor + kalimat3)
 }
 
 //3.
-for(counter=1; counter<=100; counter++){
-  if(counter%2==0){
-    console.log(counter+" even");
+for (counter = 1; counter <= 100; counter++) {
+  if (counter % 2 == 0) {
+    console.log(counter + " even");
   } else {
-    console.log(counter+" odd");
+    console.log(counter + " odd");
   }
 }
 
-for(counter=1; counter<=100; counter=counter+2){
-  if(counter%3==0){
+for (counter = 1; counter <= 100; counter = counter + 2) {
+  if (counter % 3 == 0) {
     console.log(counter + ' kelipatan' + 3);
-  }else{
+  } else {
     console.log("");
   }
 }
 
-for(counter=1; counter<=100; counter=counter+5){
-  if(counter%6==0){
-    console.log(counter +' Kelipatan ' + 6);
-  }else{
+for (counter = 1; counter <= 100; counter = counter + 5) {
+  if (counter % 6 == 0) {
+    console.log(counter + ' Kelipatan ' + 6);
+  } else {
     console.log("")
   }
 }
 
-for(counter=1; counter<=100; counter=counter+9){
-  if(counter%10==0){
+for (counter = 1; counter <= 100; counter = counter + 9) {
+  if (counter % 10 == 0) {
     console.log(counter + ' Kelipatan ' + 10)
-  }else{
+  } else {
     console.log("")
   }
 }
