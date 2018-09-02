74ecee93a5315f44d0e6c330e59342030ea774e0
Celyn Susanto <celyn.vy@gmail.com>
2018-09-02 21:03:39 +0700

    Add files via upload

--- Week 2-4.2.js
+++ Week 2-4.2.js
@@ -1,33 +1,28 @@
 //1. 
-function shoutOut(text){
-  return(text);
+function shoutOut() {
+  return 'Hello Function';
 }
-console.log(shoutOut("Hello Function"))
+console.log(shoutOut())
 
 //2.
-// function calculateMultiply(num1, num2){
-//   return num1*num2
-// }
-// var hasilPerkalian = calculateMultiply(5,6);
-// console.log(hasilPerkalian);
 
-function calculateMultiply(num1, num2){
-  return num1*num2
+function calculateMultiply(num1, num2) {
+  return num1 * num2
 }
-var num1= 5
-var num2= 6
+var num1 = 5
+var num2 = 6
 var hasilPerkalian = calculateMultiply(num1, num2);
 console.log(hasilPerkalian)
 
 //3.
-function processSentence(Name, Age, Address, Hobby){
-  return('Nama Saya ' + Name + ', umur saya ' + Age + ' tahun' + ', alamat saya di ' + Address + ', dan saya punya hobby yaitu ' + Hobby + '!')
+function processSentence(Name, Age, Address, Hobby) {
+  return ('Nama Saya ' + Name + ', umur saya ' + Age + ' tahun' + ', alamat saya di ' + Address + ', dan saya punya hobby yaitu ' + Hobby + '!')
 }
 var Name = "Agus";
 var Age = 30;
 var Address = "Jln. Malioboro, Yogjakarta";
 var Hobby = "gaming";
 
-var fullSentence= processSentence(Name, Age, Address, Hobby);
+var fullSentence = processSentence(Name, Age, Address, Hobby);
 console.log(fullSentence);
 
